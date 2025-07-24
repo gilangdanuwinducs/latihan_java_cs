0e26cfc09d1fff07dcafb6e5b65fd2da4d494bec
gilangdanuwinducs <gilangdanuwinducs@gmail.com>
2025-07-24 11:59:23 +0700

    file perubahan nama

--- DataDiri.java
+++ DataDiri.java
@@ -1,5 +1,6 @@
+
 public class DataDiri {
-    String Siswa = "Danu";
+    String Siswa = "Gilang";
 }
 
 class Kontak extends DataDiri {
@@ -10,6 +11,11 @@ class Main {
     public static void main(String[] args) {
         DataDiri daftar = new DataDiri();
         try {
+            System.out.println("Nama 1 = " + daftar.Siswa);
+            System.out.println("Nama 1 = " + daftar.Siswa);
+            System.out.println("Nama 1 = " + daftar.Siswa);
+            System.out.println("Nama 1 = " + daftar.Siswa);
+            System.out.println("Nama 1 = " + daftar.Siswa);
             System.out.println("Nama 1 = " + daftar.Siswa);
         } catch (Exception e) {
             System.out.println("Data Tidak Ada");
